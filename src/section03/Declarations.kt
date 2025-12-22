5da5276ff3d6ee962fb1e58e1eede91f70f0e2c2
Levon Tavitian <levon@tavitian.com>
2025-12-22 14:05:07 -0500

    Initial commit and Declarations notes

---
+++ src/section03/Declarations.kt
@@ -0,0 +1,21 @@
+// 19. Kotlin Variable Declarations: Understanding 'val', 'var', and Type Inference
+
+// Kotlin is statically typed
+// Kotlin can infer a variable's type from its declaration
+// val is immutable, var is mutable
+// try to use val as much as possible; declare stuff as val unless var is needed
+
+val my_int1 = 10 // type Int is inferred
+val my_int2: Int // type Int is explicit, value assigned later
+val my_int3: Int = 10 // uncommon
+
+// the class Any is the root of all classes (like Object in Java)
+// you can declare variables as Any
+
+// typealias declares a shorthand for some type
+typealias StringSet = Set<String>
+val my_string_set: StringSet // used like any other data type
+
+// equals differences:
+// ==, != : structural, defers to the .equals() method
+// ===, !=== : referential, compares to see if they're the exact same instances
